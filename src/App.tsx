356ddfa87face1ac503b7a0b3191fa6da67cd3ae
JakovlevMA <lolipopp345@gmail.com>
2023-06-06 19:52:37 +0300

    commit three

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,35 @@
-import React, {useEffect, useState} from 'react';
+import React, {useContext} from 'react';
 import {Product} from "./components/Product";
-//import {products} from "./data/products";
-import axios, {AxiosError} from "axios";
+import {useProducts} from "./hooks/products";
+import Loader from "./components/Loader";
+import ErrorMessage from "./components/ErrorMessage";
+import Modal from "./components/Modal";
+import CreateProduct from "./components/CreateProduct";
 import {IProduct} from "./modules";
-
+import {ModalContext} from "./context/ModalContext";
 
 function App() {
 
-  const [products, setProducts] = useState<IProduct[]>([])
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState('')
-  async function fetchProducts () {
-      try {
-          setError('')
-          setLoading(true)
-          const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
-          setProducts(response.data)
-          setLoading(false)
-      } catch (e: unknown) {
-          const error = e as AxiosError
-          setLoading(false)
-          setError(error.message)
-      }
-  }
-
-  useEffect(() => {
-      fetchProducts()
-  }, [])
+  const { products, loading, error , addProduct} = useProducts()
+  const {modal, open, close} = useContext(ModalContext)
+  const createHandler = (product: IProduct) => {
+      close()
+      addProduct(product)
+    }
 
   return (
       <div className='container mx-auto max-w-2xl pt-5'>
-          {loading && <p className='text-center'>Loading...</p>}
-          {error && <p className='text-center text-red-600'>{error}</p>}
-            {products.map(product => <Product product = {product} key={product.id}/>)}
+          {loading && <Loader/>}
+          {error && <ErrorMessage error={error}/>}
+          {products.map(product => <Product product = {product} key={product.id}/>)}
+          {modal && <Modal title='Create new product' onClose={close}>
+              <CreateProduct onCreate={createHandler}/>
+          </Modal>}
+
+          <button
+              className='fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2'
+              onClick={open} >
+              +</button>
       </div>
   );
 }
---
+++ src/components/CreateProduct.tsx
@@ -0,0 +1,56 @@
+import React, {useState} from 'react';
+import {IProduct} from "../modules";
+import axios from "axios";
+import ErrorMessage from "./ErrorMessage";
+
+const productData: IProduct = {
+    title: '',
+    price: 13.5,
+    description: 'lorem ipsum set',
+    image: 'https://i.pravatar.cc',
+    category: 'electronic',
+    rating: {
+        rate: 56,
+        count: 10,
+    }
+}
+
+interface createProductProps {
+    onCreate: (product: IProduct) => void
+}
+
+const CreateProduct = ({onCreate}: createProductProps) => {
+    const [value, setValue] = useState('')
+    const [error, setError] = useState('')
+    const submitHandler = async (event: React.FormEvent) => {
+        event.preventDefault()
+        setError('')
+        if (value.trim().length === 0) {
+            setError('Please enter valid title')
+            return
+        }
+
+        productData.title = value
+        const response = await axios.post<IProduct>('https://fakestoreapi.com/products', productData)
+        onCreate(response.data)
+
+    }
+
+    return (
+        <form onSubmit={submitHandler}>
+            <input
+                type="text"
+                className='border py-2 px-4 mb-2 w-full outline-0'
+                placeholder='Enter product title...'
+                value={value}
+                onChange={event => setValue(event.target.value)}
+            />
+
+            {error && <ErrorMessage error={error}/>}
+
+            <button type='submit' className='py-2 px-4 border bg-yellow-400 hover:text-blue-400'>Create</button>
+        </form>
+    );
+};
+
+export default CreateProduct;
